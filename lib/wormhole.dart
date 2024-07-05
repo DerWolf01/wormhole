d8fb85c189cc2e0f0feeb62f3b1fe85ca4b04f7f
Bilal Demirci <Bilal.demirci001@gmail.com>
2024-07-05 18:09:01 +0200

    updated main with imports

--- lib/wormhole.dart
+++ lib/wormhole.dart
@@ -6,6 +6,6 @@ export 'package:wormhole/common/messages/socket_message/socket_message.dart';
 export 'package:wormhole/common/model/model.dart';
 export 'package:wormhole/common/messages/socket_request/socket_request.dart';
 export 'package:wormhole/common/messages/socket_response/socket_response.dart';
-export 'package:wormhole/client/client.dart';
+export 'package:wormhole/client/client_socket/client_socket.dart';
 export 'package:wormhole/server/socket_server/socket_server.dart';
 #export 'common/controller/controller_service.dart';
